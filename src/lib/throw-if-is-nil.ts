9e98f0b4f44fc649938e80e6a880427a8f6756d4
고윤호 <yoonho.go@bbros.kr>
2022-01-13 17:35:05 +0900

    fix: optional to undefined in throw if is nil

--- src/lib/throw-if-is-nil.ts
+++ src/lib/throw-if-is-nil.ts
@@ -1,8 +1,8 @@
 import {throwIf} from './throw-if';
 
 interface ThrowIfIsNil {
-  <T>(err: Error, value?: T | null): T;
-  <T>(err: Error): (value?: T | null) => T;
+  <T>(err: Error, value: T | null | undefined): T;
+  <T>(err: Error): (value: T | null | undefined) => T;
 }
 
 export const throwIfIsNil: ThrowIfIsNil = <T>(err: Error, value?: T | null) => {
