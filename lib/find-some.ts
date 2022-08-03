7e90d8aa11c2fbae6bc7f7625e55f5738017b27f
고윤호 <yoonho.go@bbros.kr>
2022-08-03 14:15:25 +0900

    refactor: rename findOne and refactoring logic

--- lib/find-some.ts
+++ lib/find-some.ts
@@ -1,21 +1,29 @@
-export type Task<T> = () => Promise<T>;
+import {Predicate} from './type';
 
 /**
- * 평평한 형태의 promise tasks를 순차적으로 nil이 아닌 값을 반환하는 함수.
+ * 평평한 형태의 promise tasks를 순차적으로 조건에 맞는 것을 돌려주는 함수.
  * @example
- * const user = await findSome([
- *   () => getUser(userId),
- *   () => getUserInCache(userId),
+ * const user = await findOne(isUser, [
+ *   () => getUser(userId), // null
+ *   () => getUserInCache(userId), // null
+ *   () => getUser(email), // UserC
+ *   () => getUserInCache(email), // UserD
  * ]).then(throwIfIsNil("snip"));
+ *
+ * user === UserC
  */
-export default async <T>(tasks: Task<T>[]): Promise<T | undefined> => {
+export default async <T, E>(
+  predicate: Predicate<T | E>,
+  tasks: Array<() => Promise<T | E>>,
+  defaults: E
+): Promise<T | E> => {
   for (const task of tasks) {
     const result = await task();
 
-    if (result ?? false) {
+    if (predicate(result)) {
       return result;
     }
   }
 
-  return undefined;
+  return defaults;
 };
